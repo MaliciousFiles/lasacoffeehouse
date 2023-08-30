1fadd290ffb90774b91363cbacfef2d9e4b10f96
MaliciousFiles <mtroalson@gmail.com>
2023-08-30 11:05:47 -0500

    testing old code

--- app/performers/ClientComponent.tsx
+++ app/performers/ClientComponent.tsx
@@ -43,18 +43,20 @@ export default function ViewPerformers(props: {initialData: Stage[]}) {
         getToken(messaging, {vapidKey: "BKTiO6q1fNuQyg35h5_2PAzJhCktM0hur4llEn1gIB5Dlf6oCRCD5RIA4OY6BJvdR1UifBM22hAcKwVMc-OSUnc"})
             .then(token => {
                 if (token) {
-                    set(ref(getDatabase(firebase), `/fcm/${token}`), Date.now())
-                        .catch(() => {});
+                    alert("token")
+                    // set(ref(getDatabase(firebase), `/fcm/${token}`), Date.now())
+                    //     .catch(() => {});
                 }
             })
 
         console.log("registering messages");
         onMessage(messaging, (payload) => {
             console.log("onMessage ", payload)
-            navigator.serviceWorker.getRegistration("/firebase-cloud-messaging-push-scope")
-                .then(registration => {
-                    registration?.active?.postMessage(payload)
-                })
+            new Notification(payload.notification?.title ?? "", payload.notification);
+            // navigator.serviceWorker.getRegistration("/firebase-cloud-messaging-push-scope")
+            //     .then(registration => {
+            //         registration?.active?.postMessage(payload)
+            //     })
         });
 
     }, [notifsEnabled]);
--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -12,52 +12,55 @@ firebase.initializeApp({
     databaseURL: "https://lasacoffeehouse-74e2e-default-rtdb.firebaseio.com/"
 });
 const messaging = firebase.messaging();
-let notifsDB;
-
-(() => {
-    let request = indexedDB.open("notifications", 1);
-
-    request.onsuccess = (evt) => {
-        notifsDB = evt.target.result;
-    }
-})()
-
-function handleMessage(payload) {
-    console.log("[SW] handleMessage ", payload);
-    const {stage} = payload.data;
-    const performers = [payload.data.current, payload.data.next];
-
-    const store = notifsDB.transaction(stage, "readwrite").objectStore(stage);
-
-    for (let i = 0; i <= 1; i++) {
-        const performer = performers[i]
-
-        store.count(performer).onsuccess = (evt) => {
-            console.log(stage+"/"+performer+" = "+evt.target.result);
-            if (!evt.target.result) return;
-
-            self.registration.showNotification(!i ? "Performing Now" : "Up Next", {
-                body: !i ? `${performer} is now performing!` : `${performer} is performing next!`
-            }).then();
-        }
-    }
-    self.registration.showNotification("Received Notification!").then();
-}
-
-console.log("[SW] registering messages");
-self.addEventListener('message', evt => handleMessage(evt.data));
-self.addEventListener('activate', evt => console.log('activate', evt));
-self.addEventListener('error', evt => console.log('error', evt));
-self.addEventListener('fetch', evt => console.log('fetch', evt));
-self.addEventListener('install', evt => console.log('install', evt));
-self.addEventListener('message', evt => console.log('message', evt));
-self.addEventListener('messageerror', evt => console.log('messageerror', evt));
-self.addEventListener('notificationclick', evt => console.log('notificationclick', evt));
-self.addEventListener('notificationclose', evt => console.log('notificationclose', evt));
-self.addEventListener('offline', evt => console.log('offline', evt));
-self.addEventListener('online', evt => console.log('online', evt));
-self.addEventListener('push', evt => console.log('push', evt));
-self.addEventListener('pushsubscriptionchange', evt => console.log('pushsubscriptionchange', evt));
-self.addEventListener('rejectionhandled', evt => console.log('rejectionhandled', evt));
-self.addEventListener('unhandledrejection', evt => console.log('unhandledrejection', evt));
-messaging.onBackgroundMessage(handleMessage)
+// let notifsDB;
+//
+// (() => {
+//     let request = indexedDB.open("notifications", 1);
+//
+//     request.onsuccess = (evt) => {
+//         notifsDB = evt.target.result;
+//     }
+// })()
+//
+// function handleMessage(payload) {
+//     console.log("[SW] handleMessage ", payload);
+//     const {stage} = payload.data;
+//     const performers = [payload.data.current, payload.data.next];
+//
+//     const store = notifsDB.transaction(stage, "readwrite").objectStore(stage);
+//
+//     for (let i = 0; i <= 1; i++) {
+//         const performer = performers[i]
+//
+//         store.count(performer).onsuccess = (evt) => {
+//             console.log(stage+"/"+performer+" = "+evt.target.result);
+//             if (!evt.target.result) return;
+//
+//             self.registration.showNotification(!i ? "Performing Now" : "Up Next", {
+//                 body: !i ? `${performer} is now performing!` : `${performer} is performing next!`
+//             }).then();
+//         }
+//     }
+//     self.registration.showNotification("Received Notification!").then();
+// }
+//
+// console.log("[SW] registering messages");
+// self.addEventListener('message', evt => handleMessage(evt.data));
+// self.addEventListener('activate', evt => console.log('activate', evt));
+// self.addEventListener('error', evt => console.log('error', evt));
+// self.addEventListener('fetch', evt => console.log('fetch', evt));
+// self.addEventListener('install', evt => console.log('install', evt));
+// self.addEventListener('message', evt => console.log('message', evt));
+// self.addEventListener('messageerror', evt => console.log('messageerror', evt));
+// self.addEventListener('notificationclick', evt => console.log('notificationclick', evt));
+// self.addEventListener('notificationclose', evt => console.log('notificationclose', evt));
+// self.addEventListener('offline', evt => console.log('offline', evt));
+// self.addEventListener('online', evt => console.log('online', evt));
+// self.addEventListener('push', evt => console.log('push', evt));
+// self.addEventListener('pushsubscriptionchange', evt => console.log('pushsubscriptionchange', evt));
+// self.addEventListener('rejectionhandled', evt => console.log('rejectionhandled', evt));
+// self.addEventListener('unhandledrejection', evt => console.log('unhandledrejection', evt));
+// messaging.onBackgroundMessage(handleMessage)
+messaging.onBackgroundMessage((payload) => {
+    self.registration.showNotification(payload.notification.title, payload.notification).then();
+})
