8d3e4121b89e2d10ce121caf2a2f44424cf37887
MaliciousFiles <mtroalson@gmail.com>
2023-08-30 13:37:02 -0500

    test

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,20 +1,20 @@
 
 console.log("[SW] registering messages 1");
 
-importScripts('https://www.gstatic.com/firebasejs/10.3.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.3.0/firebase-messaging-compat.js');
-
-firebase.initializeApp({
-    apiKey: "AIzaSyBkKKxUvn9taDY1ZuRGCZOCl7t-qd5JML0",
-    authDomain: "lasacoffeehouse-74e2e.firebaseapp.com",
-    projectId: "lasacoffeehouse-74e2e",
-    storageBucket: "lasacoffeehouse-74e2e.appspot.com",
-    messagingSenderId: "381492655921",
-    appId: "1:381492655921:web:cce18985eb9f83aed36210",
-    measurementId: "G-0PZJ153E97",
-    databaseURL: "https://lasacoffeehouse-74e2e-default-rtdb.firebaseio.com/"
-});
-const messaging = firebase.messaging();
+// importScripts('https://www.gstatic.com/firebasejs/10.3.0/firebase-app-compat.js');
+// importScripts('https://www.gstatic.com/firebasejs/10.3.0/firebase-messaging-compat.js');
+//
+// firebase.initializeApp({
+//     apiKey: "AIzaSyBkKKxUvn9taDY1ZuRGCZOCl7t-qd5JML0",
+//     authDomain: "lasacoffeehouse-74e2e.firebaseapp.com",
+//     projectId: "lasacoffeehouse-74e2e",
+//     storageBucket: "lasacoffeehouse-74e2e.appspot.com",
+//     messagingSenderId: "381492655921",
+//     appId: "1:381492655921:web:cce18985eb9f83aed36210",
+//     measurementId: "G-0PZJ153E97",
+//     databaseURL: "https://lasacoffeehouse-74e2e-default-rtdb.firebaseio.com/"
+// });
+// const messaging = firebase.messaging();
 // let notifsDB;
 //
 // (() => {
@@ -64,6 +64,6 @@ self.addEventListener('pushsubscriptionchange', evt => console.log('pushsubscrip
 self.addEventListener('rejectionhandled', evt => console.log('rejectionhandled', evt));
 self.addEventListener('unhandledrejection', evt => console.log('unhandledrejection', evt));
 // messaging.onBackgroundMessage(handleMessage)
-messaging.onBackgroundMessage((payload) => {
-    self.registration.showNotification(payload.data.stage, payload.notification).then();
-})
+// messaging.onBackgroundMessage((payload) => {
+//     self.registration.showNotification(payload.data.stage, payload.notification).then();
+// })
