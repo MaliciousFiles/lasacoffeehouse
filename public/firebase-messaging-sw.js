14762f35ff82080c1f62bc1463164928a69cd785
MaliciousFiles <mtroalson@gmail.com>
2023-08-30 18:48:25 -0500

    replace `document` with `window` in SW

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -19,9 +19,7 @@ function handleMessage(payload) {
 
 // doesn't need to send notification on iOS - happens automatically
 // iOS detection taken from https://stackoverflow.com/a/9039885
-if (!['iPad Simulator', 'iPhone Simulator', 'iPod Simulator', 'iPad', 'iPhone', 'iPod']
-    .includes(navigator.platform)  &&
-    !(navigator.userAgent.includes("Mac") && "ontouchend" in window)) {
+if (!['iPad Simulator', 'iPhone Simulator', 'iPod Simulator', 'iPad', 'iPhone', 'iPod'].includes(navigator.platform)) {
     addEventListener('message', evt => handleMessage(evt.data))
     messaging.onBackgroundMessage(handleMessage);
 }
