d8705aa8009c910408e64035fdc2d135a36fab0a
Malcolm Roalson <mtroalson@gmail.com>
2023-10-27 15:07:21 -0500

    make dragging function with scrolling

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -64,23 +64,26 @@ export default function ManagePerformers() {
     const [dragging, setDragging] = useState(-1);
     const performersContainer = useRef<HTMLDivElement>(null);
     const svgOffset = useRef({x: 0, y: 0});
-    const newPerformers = useRef([] as number[]);
+    const performerPositions = useRef<number[]>([]);
 
-    const swapPerformers = (parent: HTMLElement, idx1: number, idx2: number) => {
-        if (idx2 == -1) return;
+    const scrollBy = useRef(0);
+    // const scrollTask: Promise = null;
+
+    const movePerformer = (parent: HTMLElement, idx: number, direction: 1 | -1) => {
+        let pos = performerPositions.current[idx]
+        let idx2 = performerPositions.current.indexOf(pos + direction);
 
         const marker = document.createElement("div");
-        const child1 = parent.childNodes[newPerformers.current[idx1] * 2];
-        const child2 = parent.childNodes[newPerformers.current[idx2] * 2];
+        const child1 = parent.childNodes[pos * 2];
+        const child2 = parent.childNodes[(pos + direction) * 2];
 
         parent.insertBefore(marker, child1);
         parent.insertBefore(child1, child2);
         parent.insertBefore(child2, marker);
         parent.removeChild(marker);
 
-        const temp = newPerformers.current[idx1];
-        newPerformers.current[idx1] = newPerformers.current[idx2];
-        newPerformers.current[idx2] = temp;
+        performerPositions.current[idx] += direction;
+        performerPositions.current[idx2] -= direction;
     }
 
     const startDrag = (idx: number, touchEvt?: React.TouchEvent<HTMLDivElement>, mouseEvt?: React.MouseEvent<HTMLDivElement>) => {
@@ -89,41 +92,78 @@ export default function ManagePerformers() {
 
         let pos = touchEvt ? touchEvt.touches[0] : mouseEvt!;
 
-        newPerformers.current = data[stage].performers.map((_, i)=>i);
-        svgOffset.current = {x: pos.clientX - row.offsetLeft, y: pos.clientY - row.offsetTop};
+        performerPositions.current = data[stage].performers.map((_, i)=>i);
+        svgOffset.current = {x: pos.clientX - row.offsetLeft, y: pos.clientY - row.offsetTop + performersContainer.current!.scrollTop};
         row.style.width = row.offsetWidth+'px';
     }
-    const drag = (idx: number, touchEvt?: React.TouchEvent<HTMLDivElement>, mouseEvt?: React.MouseEvent<HTMLDivElement>) => {
-        const currentIdx = newPerformers.current[idx];
 
-        if (dragging === -1) setDragging(currentIdx);
+    const checkRowMove = (row: HTMLElement, idx: number) => {
+        const currentIdx = performerPositions.current[idx];
+        const scroll = performersContainer.current!.scrollTop;
+
+        if (currentIdx > 0 && row.offsetTop + row.offsetHeight + scroll < row.parentElement!.offsetTop) {
+            movePerformer(performersContainer.current!, idx, -1);
+        } else if (currentIdx < data[stage].performers.length && row.offsetTop + scroll > row.parentElement!.offsetTop + row.parentElement!.offsetHeight) {
+            movePerformer(performersContainer.current!, idx, 1);
+        }
+    }
+    const drag = (idx: number, touchEvt?: React.TouchEvent<HTMLDivElement>, mouseEvt?: React.MouseEvent<HTMLDivElement>) => {
+        if (dragging === -1) setDragging(idx);
 
         let row = ((touchEvt || mouseEvt)!.target as HTMLElement);
         while (!row.classList.contains('table')) row = row.parentElement!;
 
-        let pos = touchEvt ? touchEvt.touches[0] : mouseEvt!;
+        let mousePosContainer = touchEvt ? touchEvt.touches[0] : mouseEvt!;
 
-        const top = (pos.clientY - (svgOffset.current.y));
-        row.style.left = (pos.clientX - (svgOffset.current.x))+'px';
+        let containerTop = performersContainer.current!.offsetTop;
+        let containerBottom = containerTop + performersContainer.current!.offsetHeight;
+
+        const setScroll = (scroll: number) => {
+            if (scrollBy.current === scroll) return;
+            scrollBy.current = scroll;
+
+            if (scroll != 0) {
+                (async () => {
+                    const container = performersContainer.current!;
+                    while (scrollBy.current != 0) {
+                        container.scrollBy({top:scrollBy.current,behavior:"smooth"});
+                        checkRowMove(row, idx);
+
+                        await new Promise(r => setTimeout(r, 50));
+                    }
+                })();
+            }
+        }
+
+        let top = Math.max(containerTop, Math.min(containerBottom - row.offsetHeight, mousePosContainer.clientY - svgOffset.current.y));
         row.style.top = top+'px';
 
-        if (currentIdx > 0 && top + row.offsetHeight < row.parentElement!.offsetTop) {
-            swapPerformers(performersContainer.current!, idx, newPerformers.current.indexOf(currentIdx-1));
-        } else if (currentIdx < data[stage].performers.length && top > row.parentElement!.offsetTop + row.parentElement!.offsetHeight) {
-            swapPerformers(performersContainer.current!, idx, newPerformers.current.indexOf(currentIdx+1));
+        let bottom = top + row.offsetHeight;
+        if (containerBottom-bottom < 10) {
+            setScroll(10);
+        } else if (top-containerTop < 10) {
+            setScroll(-10);
+        } else {
+            setScroll(0);
         }
+
+        checkRowMove(row, idx);
     }
     const stopDrag = (evt: React.UIEvent) => {
         let row = (evt.target as HTMLElement);
         while (!row.classList.contains('table')) row = row.parentElement!;
 
-        const performers = newPerformers.current.map(i => data[stage].performers[i]);
+        const performers = performerPositions.current
+            .reduce((arr, performer, idx) => {arr[performer] = idx; return arr;}, [] as number[]) // invert
+            .map(i => data[stage].performers[i]);
+
         getAuth(firebase).currentUser?.getIdToken()
             .then(jwt => reorderPerformers(jwt, stage, performers));
         setDragging(-1);
 
+        scrollBy.current = 0;
         row.style.left = row.style.top = row.style.width = '';
-        newPerformers.current = [];
+        performerPositions.current = [];
         svgOffset.current = {x: 0, y: 0};
     }
 
@@ -133,24 +173,24 @@ export default function ManagePerformers() {
 
             <Dropdown options={Object.keys(data)} onValueChanged={val => setStage(val)} />
 
-            <div ref={performersContainer} className="mt-10 text-left bg-gray-200 h-3/5 overflow-y-auto w-4/5 m-auto rounded-2xl">
+            <div ref={performersContainer} className={"shadow-inner mt-10 text-left bg-gray-200 h-3/5 overflow-y-auto w-4/5 m-auto rounded-2xl" + (dragging === -1 ? "" : " touch-none")}>
                 {([] as any[]).concat(...data[stage].performers.map((p, i) =>
                     [
-                        <div key={p+i}>
-                            {i === dragging && <div><p className={"p-3"}>&nbsp;</p></div>}
-                            <div className={"table" + (i === dragging ? " absolute bg-gray-200 rounded-2xl drop-shadow-lg z-10" : "")}>
+                        <div key={p+i} >
+                            { i === dragging && <div className={"table"}><p className={"p-3"}>&nbsp;</p></div>}
+                            <div className={"table select-none" + (i === dragging ? " fixed bg-gray-200 rounded-s shadow-2xl z-10" : "")}>
                                 <div className="table-cell whitespace-nowrap">
                                     <div
                                         onTouchMove={(evt) => drag(i, evt)}
                                         onMouseMove={(evt) => drag(i, undefined, evt)}
-                                        onTouchStart={(evt)=>startDrag(i, evt)}
-                                        onMouseDown={(evt)=>startDrag(i, undefined, evt)}
+                                        onTouchStart={(evt) => startDrag(i, evt)}
+                                        onMouseDown={(evt) => startDrag(i, undefined, evt)}
                                         onTouchEnd={stopDrag}
                                         onMouseUp={stopDrag}
-                                        className={"ml-2 inline-block translate-y-0.5"}><RiDraggable /></div>
+                                        className={"ml-2 inline-block translate-y-0.5 touch-none"}><RiDraggable /></div>
                                     <p
                                         id={"name"+i}
-                                        className={"ml-3 outline-0 inline-block p-3" + (editingName === i ? " bg-gray-300 rounded-xl" : "")}
+                                        className={"selectml-3 outline-0 inline-block p-3" + (editingName === i ? " bg-gray-300 rounded-xl" : "")}
                                         contentEditable={editingName === i}
                                         onBlur={editingName === i ? () => {new Promise(async () => {
                                             await new Promise((r) => setTimeout(r, 1));
@@ -165,18 +205,18 @@ export default function ManagePerformers() {
                                 </div>
                                 <div id="buttons" className={"table-cell w-full text-right translate-y-0.5"}>
                                     {editingName === i || removingPerformer === i ?
-                                    <div>
-                                        <button className={"mr-2 bg-green-400 text-green-200 rounded-lg p-2"}
-                                                onClick={() => confirm(true)}><FiCheck /></button>
-                                        <button className={"mr-3 bg-red-400 text-red-200 rounded-lg p-2"}
-                                                onClick={() => confirm(false)}><FiX /></button>
-                                    </div> :
-                                    <div>
-                                        <button className={"mr-2 bg-blue-400 text-blue-200 rounded-lg p-2"}
-                                                onClick={() => setEditingName(i)}><FiEdit2 /></button>
-                                        <button className={"mr-3 bg-red-400 text-red-200 rounded-lg p-2"}
-                                                onClick={() => setRemovingPerformer(i)}><FiTrash2 /></button>
-                                    </div>}
+                                        <div>
+                                            <button className={"mr-2 bg-green-400 text-green-200 rounded-lg p-2"}
+                                                    onClick={() => confirm(true)}><FiCheck /></button>
+                                            <button className={"mr-3 bg-red-400 text-red-200 rounded-lg p-2"}
+                                                    onClick={() => confirm(false)}><FiX /></button>
+                                        </div> :
+                                        <div>
+                                            <button className={"mr-2 bg-blue-400 text-blue-200 rounded-lg p-2"}
+                                                    onClick={() => setEditingName(i)}><FiEdit2 /></button>
+                                            <button className={"mr-3 bg-red-400 text-red-200 rounded-lg p-2"}
+                                                    onClick={() => setRemovingPerformer(i)}><FiTrash2 /></button>
+                                        </div>}
                                 </div>
                             </div>
                         </div>,
