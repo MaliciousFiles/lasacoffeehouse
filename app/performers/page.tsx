4242dd88605d7f56edd5a636f5e04e6b17cd5b7d
Malcolm Roalson <mtroalson@gmail.com>
2023-11-05 23:45:12 -0600

    small bug in `/performers`

--- app/performers/page.tsx
+++ app/performers/page.tsx
@@ -112,7 +112,7 @@ export default function ViewPerformers() {
                             namesDiv!.scrollTo((namesDiv!.children[i] as HTMLElement).offsetLeft-visualViewport!.width/2, 0);
                         }}>{s}</p>
                     </div>, <span key={`span${i}`} className="mx-[15px] my-0"></span>])
-                .slice(0,-1))}
+                ).slice(0,-1)}
             </div>
 
             <PerformerPopup show={showPerformers} notifsDB={notifsDB} fbToken={fbToken} performers={performers} currentPerformer={currentPerformer} currentStage={stage} close={()=>setShowPerformers(false)}></PerformerPopup>
