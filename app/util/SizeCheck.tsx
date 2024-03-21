859606fc89ace10581ad3c25927ea4aab8a99e1a
Malcolm Roalson <mtroalson@gmail.com>
2024-03-21 12:52:47 -0500

    fix size check

--- app/util/SizeCheck.tsx
+++ app/util/SizeCheck.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import React, {useEffect, useState} from "react";
+import dynamic from "next/dynamic";
 
 export default function SizeCheck(props: {children: React.ReactNode | React.ReactNode[]}) {
     const [width, setWidth] = useState(0);
 
     useEffect(() => {
         const onResize = () => setWidth(window.innerWidth);
+        onResize();
+
         window.addEventListener('resize', onResize);
         return () => window.removeEventListener('resize', onResize);
     }, []);
 
     return width <= 600 ? props.children
-        : <p>This website was developed for mobile. For the correct viewing experience, open on a phone.</p>;
+        : <p>This website was developed for mobile. For a proper viewing experience, open on a phone.</p>;
 }
