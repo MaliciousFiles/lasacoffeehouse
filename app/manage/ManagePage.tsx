d39d9371796a8420b95f8e04006933d6bc9a1c0d
Malcolm Roalson <mtroalson@gmail.com>
2025-05-14 07:46:51 -0500

    debug

--- app/manage/ManagePage.tsx
+++ app/manage/ManagePage.tsx
@@ -35,7 +35,9 @@ export default function ManagePage() {
         const activeString = JSON.stringify(activePerformers.map(p => p.uid));
 
         // the user did something, therefore update clients with new data
+        alert(oldActivePerformers+" vs "+activeString);
         if (oldActivePerformers !== undefined && oldActivePerformers != activeString) {
+            alert("updating");
             updateFirebase(jwt => updateClients(jwt, stage, activePerformers[0], activePerformers[1]), false);
         } else {
             // since they didn't update it, visual scroll
